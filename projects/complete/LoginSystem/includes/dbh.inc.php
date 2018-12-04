51e511a1be66e2dc328d668ed06617fbab99c279
Braydon Turner <braydonturn@gmail.com>
2018-12-04 13:02:57 -0500

    Create dbh.inc.php

---
+++ projects/complete/LoginSystem/includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "id7364572_root";
+$dbPassword = "dream100";
+$dbName = "id7364572_loginsystem";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
