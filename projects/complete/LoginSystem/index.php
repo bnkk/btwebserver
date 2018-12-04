06f0c70cf48b975fb4331c8686c5d56e304090a6
Braydon Turner <braydonturn@gmail.com>
2018-12-04 12:58:36 -0500

    Create index.php

---
+++ projects/complete/LoginSystem/index.php
@@ -0,0 +1,21 @@
+<?php
+	include_once 'header.php';
+?>
+
+<section class="main-container">
+	<div class="main-wrapper">
+		<h2>Login To Continue!</h2>
+		<?php
+        	if (isset($_SESSION['u_id'])) {
+        		echo "You are logged in!";
+        	}
+		?>
+	</div>
+</section>
+
+<?php
+	include_once 'footer.php';
+?>
+
+</body>
+</html>
